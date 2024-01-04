fe02654c2b547c39a3250fc0ecc738790412b81b
eunicehassan3 <euniceh.info@gmail.com>
2024-01-03 23:47:56 -0600

    created the rest of the html files

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ from flask import (
 # create Flask App
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='1020')
